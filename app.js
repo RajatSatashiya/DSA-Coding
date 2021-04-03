8c43ae24bc3475e499b2f5afe4f8dc4f016be3d8
Rajat Satashiya <kshri4398@gmail.com>
2021-04-03 16:55:52 +0530

    mega changes

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const culturePic = document.querySelector(".slideshow");
+const items = document.querySelector(".nav-items");
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const college = document.querySelector(".college");
+const hideLogo = document.querySelector(".hide");
+const navBar = document.querySelector(".nav-bar");
 var i = -1;
 
 const imgArray = [
@@ -35,3 +41,41 @@ if (screen.width < 600) {
 } else {
   images(imgArray, 5000);
 }
+
+navToggle.addEventListener("click", () => {
+  items.classList.toggle("show-items");
+  const containerHeight = items.getBoundingClientRect().height;
+  const linkHeight = links.getBoundingClientRect().height;
+
+  if (containerHeight === 0) {
+    items.style.height = `${linkHeight}px`;
+  } else {
+    items.style.height = 0;
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= college.offsetTop) {
+    hideLogo.style.visibility = "visible";
+  } else if (window.scrollY < college.offsetTop) {
+    hideLogo.style.visibility = "hidden";
+  }
+});
+
+const scrollLink = document.querySelectorAll(".scroll-link");
+
+scrollLink.forEach(function (link) {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position = element.offsetTop;
+    window.scrollTo({
+      left: 0,
+      top: position - navBar.getBoundingClientRect().height,
+    });
+
+    items.style.height = 0;
+  });
+});
