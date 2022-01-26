7577bbbe5c9d184c772e05181db8dcf5dd2ec982
Rajat Satashiya <kshri4398@gmail.com>
2022-01-26 22:11:08 +0530

    Time: 19 ms (86.58%), Space: 43.3 MB (64.96%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,21 +1,25 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] num) {
-    Arrays.sort(num);
-    List<List<Integer>> res = new LinkedList<>(); 
-    for (int i = 0; i < num.length-2; i++) {
-        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-            while (lo < hi) {
-                if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                    while (lo < hi && num[lo] == num[lo+1]) lo++;
-                    while (lo < hi && num[hi] == num[hi-1]) hi--;
-                    lo++; hi--;
-                } else if (num[lo] + num[hi] < sum) lo++;
-                else hi--;
-           }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new LinkedList<>();
+        Arrays.sort(nums);
+
+        for (int i=0; i < nums.length-2; i++) {
+            if (i==0 ||  (i > 0 && nums[i] != nums[i-1])) { 
+                int first = -nums[i], lo = i+1, hi = nums.length - 1;
+                while(lo < hi) {
+                    if(nums[lo] + nums[hi] == first) {
+                        ans.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+                        while(lo < hi && nums[lo] == nums[lo+1]) lo++;
+                        while(lo < hi && nums[hi] == nums[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (nums[lo] + nums[hi] > first) {
+                        hi--;
+                    } else {
+                        lo++;
+                    }
+                }
+            }
         }
+        return ans;
     }
-    return res;
-}
 }
