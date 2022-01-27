ffc5b5ca962e8ee1d2f0e8e292ee639c3fe3ec8b
Rajat Satashiya <kshri4398@gmail.com>
2022-01-27 14:42:39 +0530

    Time: 1 ms (92.00%), Space: 40.7 MB (5.92%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -5,8 +5,10 @@ class Solution {
         //-3,3,3,90
         
         int count = 0;
+        int smallest = nums[0];
+        int largest = nums[nums.length-1];
         for(int i=1; i<nums.length-1; i++){
-            if(nums[0] < nums[i] && nums[i] < nums[nums.length-1]){
+            if(smallest < nums[i] && nums[i] < largest){
                 count++;
             }
         }
