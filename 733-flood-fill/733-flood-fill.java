745bfe8687ec69f93725475c235dacbd58054cf7
Rajat Satashiya <kshri4398@gmail.com>
2022-02-22 22:49:24 +0530

    Time: 2 ms (33.27%), Space: 48.3 MB (8.10%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,19 +1,24 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        if(image[sr][sc] == newColor) return image;
-        kardoBeta(image, sr, sc, image[sr][sc], newColor);
+        // if(image[sr][sc] == newColor) return image;
+        boolean[][] arr = new boolean[image.length][image[0].length];
+        kardoBeta(image, sr, sc, image[sr][sc], newColor, arr);
         return image;
     }
     
-    public static void kardoBeta(int[][] image, int sr, int sc, int color, int newColor){
-        if(sr < 0 || sr == image.length || sc < 0 || sc == image[0].length || image[sr][sc] != color){
+    public static void kardoBeta(int[][] image, int sr, int sc, int color, int newColor, boolean[][] arr){
+        if(sr < 0 || sr == image.length || sc < 0 || sc == image[0].length || image[sr][sc] != color || arr[sr][sc] == true){
             return;
         }
         image[sr][sc] = newColor;
+        arr[sr][sc] = true;
         
-        kardoBeta(image, sr + 1, sc, color, newColor);
-        kardoBeta(image, sr - 1, sc, color, newColor);
-        kardoBeta(image, sr, sc + 1, color, newColor);
-        kardoBeta(image, sr, sc - 1, color, newColor);
+        kardoBeta(image, sr + 1, sc, color, newColor, arr);
+        kardoBeta(image, sr - 1, sc, color, newColor, arr);
+        kardoBeta(image, sr, sc + 1, color, newColor, arr);
+        kardoBeta(image, sr, sc - 1, color, newColor, arr);
     }
 }
+
+// 0 0 0
+// 0 1 1
