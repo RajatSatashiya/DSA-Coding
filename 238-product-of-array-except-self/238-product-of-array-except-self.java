193f6e625ad79ce396f7c9cd6eb94eb48d401c9f
Rajat Satashiya <kshri4398@gmail.com>
2022-03-14 21:40:34 +0530

    Time: 246 ms (5.12%), Space: 68.2 MB (5.51%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int prod = 1, count = 0;
-        boolean isZero = false;
-        int[] ans = new int[nums.length];
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int[] ans    = new int[nums.length];
         
-        for(int i:nums){
-            if(i != 0){
-                prod *= i;
-            } else { 
-                count++;
-                isZero = true;
-            }
+        prefix[0] = 1;
+        //calculate every prefix sum
+        for(int i=1; i<nums.length; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
         }
+
+        suffix[nums.length - 1] = 1;
+        //calculate every suffix sum
+        for(int i = nums.length - 2; i >= 0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+        
+        for(int i:suffix){
+            System.out.println(i);
+        }
+        
+        //multiple both
         for(int i=0; i<nums.length; i++){
-            if(count >= 2){
-                ans[i] = 0;
-                continue;
-            }
-            if(isZero && nums[i] != 0){
-                ans[i] = 0;
-                continue;
-            }
-            if(nums[i] == 0){
-                ans[i] = prod;
-            } else {
-                ans[i] = prod / nums[i];
-                // System.out.println(ans[i]);
-            }
+            ans[i] = prefix[i] * suffix[i];
         }
+        
         return ans;
     }
 }
