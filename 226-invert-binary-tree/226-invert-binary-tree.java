1c41ff03c63c6e1f0007f70721e56b4005b2a0a6
Rajat Satashiya <kshri4398@gmail.com>
2022-04-16 12:14:10 +0530

    Time: 0 ms (100.00%), Space: 39.8 MB (90.20%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -1,12 +1,35 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return null;
+        /*if(root == null) return null;
         
         TreeNode left = root.left, right = root.right;
         
         root.left = invertTree(right);
         root.right = invertTree(left);
         
+        
+        return root;*/
+        
+        
+        //*** stack solution***//
+            
+        if(root == null) return null;
+        Stack<TreeNode> st = new Stack<>();
+        st.push(root);
+        
+        while(!st.isEmpty()){
+            TreeNode temp = st.pop();
+            TreeNode left = temp.left;
+            temp.left = temp.right;
+            temp.right= left;
+            
+            if(temp.left != null){
+                st.push(temp.left);
+            }
+            if(temp.right != null){
+                st.push(temp.right);
+            }
+        }
         
         return root;
     }
