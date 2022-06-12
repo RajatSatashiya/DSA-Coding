127e2978e5891d6da11165647f176ce035dbc5db
Rajat Satashiya <kshri4398@gmail.com>
2022-06-12 13:24:56 +0530

    Time: 166 ms (10.31%), Space: 120.4 MB (43.64%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        
+        for(int i=0; i < nums.length; i++) {
+            map.put(nums[i],false);
+        }
+        
+        int lo = 0, hi = 0, sum = 0, ans = 0;
+        while(lo <= hi && hi < nums.length){
+            // System.out.println(nums[hi]);
+            if(!map.get(nums[hi])) {
+                sum += nums[hi];
+                ans = Math.max(ans, sum);
+                map.put(nums[hi], true);
+                hi++;
+            } else {
+                // sum -= nums[lo];
+                // lo++;
+                // if(nums[lo] == nums[hi]) {
+                    // map.put(nums[lo], false);
+                // }
+                sum -= nums[lo];
+                map.put(nums[lo], false);
+                lo++;
+                
+            }
+            // System.out.println("sum: " + sum);
+        }
+        return ans;
+    }
+}
