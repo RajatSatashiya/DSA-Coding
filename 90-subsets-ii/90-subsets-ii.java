845e09b654deb5b25f918eae1443812abb3269dc
Rajat Satashiya <kshri4398@gmail.com>
2022-06-15 21:32:49 +0530

    Time: 2 ms (79.55%), Space: 42.4 MB (90.37%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,19 +1,14 @@
 class Solution {
     HashSet<List<Integer>> set = new HashSet<>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        // Arrays.sort(nums, (a,b) -> Integer.compare(b , a));
         Arrays.sort(nums);
         recursion(nums, 0);
         
-        // System.out.println("set: " + set);
         List<List<Integer>> ans = new ArrayList<>();
         ans.addAll(set);
         return ans;
     }
     
-    //[1,2,3]
-    // 1 + comb(2,3);
-    // 1 + (2 + comb(3)); === ([] , [3]) + 2 === ([2], [2,3], [], [3])
     public void recursion(int[] nums, int start) {
         if(start == nums.length - 1) {
             List<Integer> temp = new ArrayList<>();
@@ -30,12 +25,12 @@ class Solution {
             List<Integer> arr = new ArrayList<>(li);
             arr.add(nums[start]);
             set2.add(arr);
-            // System.out.println("arr: " + arr);
         }
         
-        for(List<Integer> li:set2) {
-            set.add(li);
-        }
+        // for(List<Integer> li:set2) {
+            // set.add(li);
+        // }
+        set.addAll(set2);
         return;
     }
 }
