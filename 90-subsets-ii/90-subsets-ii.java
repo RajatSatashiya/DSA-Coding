d4edd1d3c8e9c84e384f8b66192e6c16ca58df6d
Rajat Satashiya <kshri4398@gmail.com>
2022-06-15 21:32:00 +0530

    Time: 6 ms (13.85%), Space: 44 MB (50.67%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,30 +1,41 @@
 class Solution {
-    HashSet<List<Integer>> hs=new HashSet();
-    public void sub(int i,int[] nums,List<Integer> l){
-        if(i==nums.length){
-            // System.out.println(l);
-            List<Integer> arr = new ArrayList<>(l);
-            Collections.sort(arr);
-            hs.add(arr);
+    HashSet<List<Integer>> set = new HashSet<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // Arrays.sort(nums, (a,b) -> Integer.compare(b , a));
+        Arrays.sort(nums);
+        recursion(nums, 0);
+        
+        // System.out.println("set: " + set);
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.addAll(set);
+        return ans;
+    }
+    
+    //[1,2,3]
+    // 1 + comb(2,3);
+    // 1 + (2 + comb(3)); === ([] , [3]) + 2 === ([2], [2,3], [], [3])
+    public void recursion(int[] nums, int start) {
+        if(start == nums.length - 1) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(nums[start]);
+            set.add(temp);
+            set.add(new ArrayList<>());
             return;
         }
-        else{
-           
-            sub(i+1,nums,l);
-            l.add(nums[i]);
-            sub(i+1,nums,l);
-            l.remove(l.size()-1);
-            
-            
+        
+        recursion(nums, start + 1);
+        
+        HashSet<List<Integer>> set2 = new HashSet<>();
+        for(List<Integer> li:set) {
+            List<Integer> arr = new ArrayList<>(li);
+            arr.add(nums[start]);
+            set2.add(arr);
+            // System.out.println("arr: " + arr);
+        }
+        
+        for(List<Integer> li:set2) {
+            set.add(li);
         }
         return;
-    }
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<Integer> l=new ArrayList<Integer>();
-        sub(0,nums,l);
-        List<List<Integer>> ll=new ArrayList<List<Integer>>();
-        ll.addAll(hs);
-        return ll;
-        
     }
 }
