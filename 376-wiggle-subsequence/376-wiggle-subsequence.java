a6fa773bbd28f3bc11d70502cddfeacdf74aec6d
Rajat Satashiya <kshri4398@gmail.com>
2022-07-04 13:23:10 +0530

    Time: 1 ms (33.33%), Space: 42 MB (24.25%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -1,18 +1,25 @@
-public class Solution {
+class Solution {
     public int wiggleMaxLength(int[] nums) {
-        if (nums.length < 2)
-            return nums.length;
-        int[] up = new int[nums.length];
-        int[] down = new int[nums.length];
-        for (int i = 1; i < nums.length; i++) {
-            for(int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    up[i] = Math.max(up[i],down[j] + 1);
-                } else if (nums[i] < nums[j]) {
-                    down[i] = Math.max(down[i],up[j] + 1);
-                }
+        if(nums.length == 1) {
+            return 1;
+        }
+        int[][] dp = new int[nums.length][2];
+        dp[0][0] = 1;
+        dp[0][1] = 1;
+        
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] - nums[i-1] > 0) {
+                dp[i][0] = dp[i-1][1] + 1;
+                dp[i][1] = dp[i-1][1];
+            } else if(nums[i] - nums[i-1] < 0) {
+                dp[i][0] = dp[i-1][0];
+                dp[i][1] = dp[i-1][0] + 1;
+            } else {
+                dp[i][1] = dp[i-1][1];
+                dp[i][0] = dp[i-1][0];
             }
         }
-        return 1 + Math.max(down[nums.length - 1], up[nums.length - 1]);
+        
+        return Math.max(dp[dp.length - 1][0], dp[dp.length - 1][1]);
     }
 }
