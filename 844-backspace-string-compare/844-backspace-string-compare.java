91e09a662929cc7e435a3bdcb9794224534c83ea
Rajat Satashiya <kshri4398@gmail.com>
2022-07-06 01:00:19 +0530

    Time: 3 ms (50.42%), Space: 41.7 MB (79.27%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,38 +1,36 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> st1 = new Stack<>();
-        Stack<Character> st2 = new Stack<>();
+        Stack<Character> st = new Stack<>();
         
         //for s
         for(char ch:s.toCharArray()) {
-            // System.out.println("before: " + st1);
             if(ch == '#') {
-                if(!st1.isEmpty()) {
-                    st1.pop();                    
+                if(!st.isEmpty()) {
+                    st.pop();                    
                 }
             } else {
-                st1.push(ch);
-            }
-            // System.out.println("after: " + st1);
-        }
-        //for t
-        for(char ch:t.toCharArray()) {
-            if(ch == '#') {
-                if(!st2.isEmpty()) {
-                    st2.pop();                    
-                }
-            } else {
-                st2.push(ch);
+                st.push(ch);
             }
         }
         
-        // System.out.println(st1);
-        // System.out.println(st2);
-        //final comparison
-        if(st1.size() != st2.size()) return false;
-        while(!st1.isEmpty()) {
-            if(st1.pop() != st2.pop()) return false;
+        int hash = 0;
+        for(int i = t.length() - 1; i >= 0; i--) {
+            if(t.charAt(i) == '#') {
+                hash++;
+                continue;
+            }
+            if(hash > 0) {
+                hash--;
+                continue;
+            } else {
+                if(!st.isEmpty() && st.pop() == t.charAt(i)) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+            
         }
-        return true;
+        return st.isEmpty();
     }
 }
