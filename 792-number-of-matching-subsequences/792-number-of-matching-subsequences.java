ce2cbd6ccc93c1f1f020a7643ba0a2fcde454604
Rajat Satashiya <kshri4398@gmail.com>
2022-07-21 15:15:33 +0530

    Time: 2982 ms (5.04%), Space: 118.2 MB (19.65%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        List<Integer>[] pos = new List['z'-'a'+1];
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if(pos[ch - 'a'] == null) {
+                pos[ch - 'a'] = new ArrayList<>();
+            }
+            pos[ch - 'a'].add(i);
+        }
+        
+        //[a,b,c,d,e] -> [[1], [2], [3], [4], [5]];
+        int ans = 0;
+        
+        for(String str:words) {
+            int cur = 0;
+            for(int i = 0; i < str.length(); i++, cur++) {
+                char ch =  str.charAt(i);
+                System.out.println("char: " + ch + " " + cur);
+                cur = search(pos[ch - 'a'], cur);
+                if(cur == -1) break;
+            }
+            if(cur != -1) {
+                ans++;
+            }
+        }
+        return ans;        
+    }
+    
+    public int search(List<Integer> pos, int cur) {
+        if(pos == null) return -1;
+        int lo = 0, hi = pos.size() - 1;
+        
+        if(pos.get(lo) > cur) return pos.get(lo);
+        if(pos.get(hi) < cur) return -1;
+        
+        while(lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(pos.get(mid) == cur) {
+                System.out.println("yes");
+                return pos.get(mid);
+            }
+            if(pos.get(mid) < cur) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return pos.get(hi);
+    }
+    
+}
