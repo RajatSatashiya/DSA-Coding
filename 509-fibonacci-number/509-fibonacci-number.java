77fa905b2ea0db18d1c8d78bbf28a23d7e9e5997
Rajat Satashiya <kshri4398@gmail.com>
2022-08-08 23:45:51 +0530

    Time: 31 ms (5.16%), Space: 40.2 MB (83.24%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,20 @@
 class Solution {
+    int[] dp;
     public int fib(int n) {
+        dp = new int[n + 1];
         return recursion(n);
     }
     
     public int recursion(int n) {
         if(n == 0 || n == 1) {
+            dp[n] = n;
             return n;
         }
+        if(dp[n] != 0) {
+            return dp[n];
+        }
         
-        return recursion(n - 1) + recursion(n - 2);
+        int ans = recursion(n - 1) + recursion(n - 2);
+        return ans;
     }
 }
