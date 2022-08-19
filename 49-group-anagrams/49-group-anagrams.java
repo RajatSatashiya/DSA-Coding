edd275b5f95e2409e8f6d5eec99319279856b9a0
Rajat Satashiya <kshri4398@gmail.com>
2022-08-19 12:53:18 +0530

    Time: 35 ms (21.07%), Space: 63 MB (13.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,35 +1,28 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        //eat, (ate,0) [[0,1,3], [2]]
-        //tea, 
-        //tan, (ant,2)
-        HashMap<String, Integer> map = new HashMap<>();
-        ArrayList<ArrayList<Integer>> li = new ArrayList<>();
-        int mapCount = 0;
+        List<List<String>> li = new ArrayList<>();
+        Map<String, ArrayList<Integer>> map = new HashMap<>();
         
-        for(int i=0; i<strs.length; i++){
+        for(int i = 0; i < strs.length; i++) {
             char[] ch = strs[i].toCharArray();
             Arrays.sort(ch);
-            String str = new String(ch);
             
-            if(map.containsKey(str)){
-                int key = map.get(str);
-                li.get(key).add(i);
-            } else {
-                map.put(str,mapCount++);
-                li.add(new ArrayList<>());
-                li.get(mapCount-1).add(i);
+            //convert array to string
+            String str = "";
+            for(char temp : ch) {
+                str += temp;
             }
+            map.computeIfAbsent(str , name -> new ArrayList<>()).add(i);
         }
-        List<List<String>> li2 = new ArrayList<>();
         
-        for(int i=0; i<li.size(); i++){
-            li2.add(new ArrayList<>());
-            for(int j=0; j<li.get(i).size(); j++){
-                String val = strs[li.get(i).get(j)];
-                li2.get(i).add(val);
+        for(String str : map.keySet()) {
+            List<String> arr = new ArrayList<>();
+            for(int i : map.get(str)) {
+                arr.add(strs[i]);
             }
+            li.add(new ArrayList<>(arr));
         }
-        return li2;
+        return li;
     }
+    
 }
