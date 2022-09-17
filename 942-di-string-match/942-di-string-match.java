5d44e6577f1c23d96dee24772b386aa1b471ee0c
Rajat Satashiya <kshri4398@gmail.com>
2022-09-17 17:16:42 +0530

    Time: 4 ms (49.44%), Space: 48.4 MB (47.78%) - LeetHub

--- 942-di-string-match/942-di-string-match.java
+++ 942-di-string-match/942-di-string-match.java
@@ -3,26 +3,16 @@ class Solution {
         int[] temp = new int[s.length() + 1];
         
         int lo = 0, hi = s.length();
-        HashSet<Integer> set = new HashSet<>();
         
         for(int i = 0; i < s.length(); i++) {
             if(s.charAt(i) == 'I') {
-                set.add(lo);
-                temp[i] = lo;
-                lo++;
+                temp[i] = lo++;
             } else {
-                set.add(hi);
-                temp[i] = hi;
-                hi--;
+                temp[i] = hi--;
             }
         }
         
-        for(int i = 0; i <= s.length(); i++) {
-            if(!set.contains(i)) {
-                temp[temp.length - 1] = i;
-            }
-        }
-        
+        temp[s.length()] = lo;
         return temp;
     }
 }
