3ebb29abbaf20aaa7fdf0094a90ef22ee4c84a69
Rajat Satashiya <kshri4398@gmail.com>
2022-09-17 17:19:42 +0530

    Time: 6 ms (20.08%), Space: 48.5 MB (27.78%) - LeetHub

--- 942-di-string-match/942-di-string-match.java
+++ 942-di-string-match/942-di-string-match.java
@@ -1,28 +1,20 @@
 class Solution {
     public int[] diStringMatch(String s) {
-        int[] temp = new int[s.length() + 1];
+        int N = s.length();
+        int[] perm = new int[N+1];
+        int lo = 0, hi = N;
         
-        int lo = 0, hi = s.length();
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'I') {
-                temp[i] = lo++;
-            } else {
-                temp[i] = hi--;
+        for(int i = 0; i < N; i++){
+            if(s.charAt(i) == 'I'){
+                perm[i]=lo;
+                lo++;
+            } else{
+                perm[i]=hi;
+                hi--;
             }
         }
         
-        temp[s.length()] = lo;
-        return temp;
+        perm[N] = lo;
+        return perm;
     }
 }
-
-/*
-D -> lo
-I -> hi
-
-0
-lo = 0
-hi = 4
-
-*/
